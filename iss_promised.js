4bd69122f3c503d712d66ff2c24e39cac773e638
Jehanne Hodge <samarahjhodge@gmail.com>
2020-06-04 22:36:22 -0400

    linting errors

--- iss_promised.js
+++ iss_promised.js
@@ -11,7 +11,7 @@ const fetchCoordsByIP = function(body) {
 
 const fetchISSFlyOverTimes = function(body) {
   const { latitude, longitude } = JSON.parse(body).data;
-  const url = `http://api.open-notify.org/iss-pass.json?lat=${latitude}&lon=${longitude}`
+  const url = `http://api.open-notify.org/iss-pass.json?lat=${latitude}&lon=${longitude}`;
   return request(url);
 };
 
@@ -22,7 +22,7 @@ const nextISSTimesForMyLocation = function() {
     .then((data) => {
       const { response } = JSON.parse(data);
       return response;
-  });
+    });
 };
 
 const printPassTimes = function(passTimes) {
